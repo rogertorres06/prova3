aaeb00798b7d3e22a7119702884f6deb9c48b5b2
rogertorres06 <roger.torresny@gmail.com>
2023-01-19 15:14:47 -0300

    subii

---
+++ paciente/src/main/java/br/com/tech4me/paciente/PacienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.tech4me.paciente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacienteApplication.class, args);
+	}
+
+}
