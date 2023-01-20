87b336b5797c09486b6ec03240719cd0e376f487
wendel-correa <correa.wendel@gmail.com>
2023-01-20 15:28:51 -0300

    ajuste na pasta

---
+++ procedimentos/.history/src/main/java/br/com/tech4me/procedimentos/httpclient/ProcedimentosClient_20230120152605.java
@@ -0,0 +1,15 @@
+package br.com.tech4me.procedimentos.httpClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+import br.com.tech4me.procedimentos.model.Paciente;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient("paciente")
+public interface ProcedimentosClient {
+    
+    @RequestMapping(method =RequestMethod.GET, value="/paciente/{id}")
+    Paciente obterPaciente(@PathVariable String id);
+}
